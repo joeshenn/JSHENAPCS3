29a605d4be8271be021708ad018e75d5aa3ae636
APCS3rd <APCS3rd@MHS-Math2-21>
2018-08-20 10:43:43 -0700

    First commit to github

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,17 @@
+
+/**
+ * Prints "Hello World!"
+ * 
+ * @author JoeShen
+ * @version (8/17/2018)
+ */
+	public class Hello_world
+	{
+	    public static void main(String[] args)
+	    {
+	        System.out.println("Hello World! From Joe"); //Display the string
+	    }
+	}
+
+
+
