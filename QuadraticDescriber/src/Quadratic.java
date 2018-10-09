d8433a1518884dd11e502360fc35a1e1a3e7fa1f
APCS3rd <APCS3rd@MHS-Math2-21>
2018-10-09 12:25:36 -0700

    classwork | finished part 5

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -5,9 +5,12 @@
  * @version 10/2/2018
  */
 public class Quadratic {
+	//the method discriminant takes three doubles as inputs and returns the discriminant, as if 
+	//the inputs were the coefficients of a quadratic equation in standard form.
 	public static double discriminant(double a, double b, double c) {
 		return (b*b)-(4*a*c);
 	}
+	//the method min outputs the lesser of two double inputs.
 	public static double min(double num1, double num2) {
 		if(num1 <= num2) {
 			return num1;
@@ -15,6 +18,7 @@ public class Quadratic {
 			return num2;
 		}
 	}
+	//the method max returns the greater of two double inputs.
 	public static double max(double num1, double num2) {
 		if(num1 >= num2) {
 			return num1;
@@ -22,6 +26,7 @@ public class Quadratic {
 			return num2;
 		}
 	}
+	//the method absValue returns the absolute value of the input.
 	public static double absValue(double num) {
 		if(num >= 0) {
 			return num;
@@ -30,6 +35,7 @@ public class Quadratic {
 			return num*-1;
 		}
 	}
+	//the method round2 returns the value of the input rounded to the hundredths place
 	public static double round2(double num1) {
 		int thousandTimes = (int) (num1 * 1000);
 		int hundredTimes = (int)(num1*100);
@@ -53,6 +59,7 @@ public class Quadratic {
 		}
 			
 		}
+	//the method sqrt returns the square root of the input
 public static double sqrt(double num) {
 		if(num<0) {
 			throw new IllegalArgumentException("negative numbers are not accepted");
@@ -66,6 +73,7 @@ public static double sqrt(double num) {
 		return round2(answer1);
 			}
 }
+//the method quadForm determines the roots of a quadratic equation, based on the coefficient inputs.
 public static String quadForm(double a, double b, double c) {
 	double discrim= discriminant(a, b, c);
 	if(discrim<0) {
@@ -79,11 +87,13 @@ public static String quadForm(double a, double b, double c) {
 		return "There are two x intercepts at "+"("+ min(root1, root2)+",0) and (" + max(root1, root2)+",0)";
 	}
 }
+//the method quadrDescriber returns the direction of opening, location of vertex, and intercepts of a
+//quadratic equation
 	public static String quadrDescriber(double a, double b, double c) {
 		String opening;
 		double xvert = (-1*b)/(2*a);
 		double yvert = (a*xvert*xvert) + (b*xvert) + c;
-		String vertex= "The vertex is located at the point("+xvert+","+yvert+".";
+		String vertex= "The vertex is located at the point("+xvert+","+yvert+")"+".";
 		String intercepts = quadForm(a, b, c) ;
 		if(a>0) {
 			opening="The graph opens upwards.";
@@ -91,7 +101,7 @@ public static String quadForm(double a, double b, double c) {
 		else {
 			opening="The graph opens downwards.";
 		}
-		return opening +"\n"+ vertex +"\n"+ intercepts;
+		return "Description of the graph y="+a+"x^2+"+b+"x+"+c+ "\n"+opening +"\n"+ vertex +"\n"+ intercepts;
 	}
 }
 
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,13 +1,41 @@
-/* This class receives user input detailing the co-efficients of a quadratic equation and outputs a 
+/* This class receives user input detailing the coefficients of a quadratic equation and outputs a 
  * description of the graph which is generated from Quadratic
  * @author Joe Shen
  * @version 10/2/2018
  */
 import java.util.*;
 public class QuadraticClient {
-	Scanner userInput = new Scanner(System.in);
+	//the main method prompts the user for the coefficients and calls the method quadrDescriber from
+	//the class Quadratic to output an answer. 
 	public static void main(String[] args) {
-		
-		
+		boolean repeat=true;
+		while(repeat==true) {
+		Scanner userInput = new Scanner (System.in);
+		System.out.println("Please provide the coefficients of a quadratic equation in standard form" );
+		System.out.print("a: ");
+		double a = userInput.nextDouble();
+		System.out.print("b: ");
+		double b = userInput.nextDouble();
+		System.out.print("c: ");
+		double c = userInput.nextDouble();
+		if(a==0) {
+			System.out.println("The coefficient \"a\"cannot equal 0.\nTry again? (y/n)");
+			if(userInput.next().equals("y")) {
+				repeat = true;
+			}else {
+				repeat = false;
+				userInput.close();
+			}
+		}else {
+			System.out.println(Quadratic.quadrDescriber(a, b, c));
+			System.out.println("Would you like to try again?(y/n)");
+			if(userInput.next().equals("y")) {
+				repeat = true;
+			}else {
+				repeat = false;
+				userInput.close();
+			}
+		}
 	}
+}
 }
