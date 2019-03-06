ea658f759fb7a27b3614fec5e81e649e756797df
Joe <Joe@Fifi-pc>
2019-03-05 23:57:00 -0800

    check 1

--- TextExcel/src/textExcel/SpreadsheetLocation.java
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -6,36 +6,38 @@ public class SpreadsheetLocation implements Location
 {
 	private String location;
 	private char[] input;
+    public SpreadsheetLocation(String cellName)
+    {
+        // TODO: Fill this out with your own code
+    	location = cellName.toUpperCase();
+    	input = new char[location.length()];
+    	for(int i=0;i<location.length();i++) {
+    		input[i]=location.charAt(i);
+    		}
+    	}
     @Override
     public int getRow()
     {
         // TODO Auto-generated method stub
-    	if (location.length()==2){
-            return input[1];
-    	}
-    	else if (location.length()==3) {
-    	return Integer.parseInt((""+input[1])+(""+input[2]))-1;
-    	}
-    	else {
-    		return 0;
-    	}
+        return input[0]-65;
+    	
     }
 
     @Override
     public int getCol()
     {
         // TODO Auto-generated method stub
-        return input[0]-65;
-    }
-    
-    public SpreadsheetLocation(String cellName)
-    {
-        // TODO: Fill this out with your own code
-    	location = cellName.toUpperCase();
-    	for(int i=0;i<location.length();i++) {
-    		input[i]=location.charAt(i);
-    		}
+    	if (location.length()==2){
+            return input[1];
     	}
+    	else if (location.length()==3) {
+    	return Integer.parseInt((""+input[1])+(""+input[2])) - 1;
+    	}
+    	else {
+    		return 0;
+    	}
+
+    }
     }
 
 
