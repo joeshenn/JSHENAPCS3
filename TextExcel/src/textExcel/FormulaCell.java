8fad962e63b08e2eb5e56a8e0b95166997404537
APCS3rd <APCS3rd@MHS-Math2-21>
2019-03-28 10:51:09 -0700

    Finished check 4 | work on check 5

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -6,14 +6,32 @@ public class FormulaCell extends RealCell {
 	}
 	public double getDoubleValue(){
 		String[] longInput = (super.fullCellText()).split(" ");
-		for(int i=1;i<=longInput.length-2;i+=2) {
+		for(int i=1;i<longInput.length-2;i+=2) {
 			longInput[i+2] = produceAnswer(longInput[i]+" "+longInput[i+1]+" "+longInput[i+2]);
 	}
-		//placeholder
-		return 0.0;
+		return Double.parseDouble(longInput[longInput.length-2]);
 	}
 	public static String produceAnswer(String input) {
-		//placeholder
-		return "hi";
+		String[] calculate = input.split(" ",3);
+		if(calculate[1].equals("+")) {
+			return (Double.parseDouble(calculate[0]) + Double.parseDouble(calculate[2]))+"";
+		}
+		else if(calculate[1].equals("-")) {
+			return (Double.parseDouble(calculate[0]) - Double.parseDouble(calculate[2]))+"";
+		}
+		else if(calculate[1].equals("*")) {
+			return (Double.parseDouble(calculate[0]) * Double.parseDouble(calculate[2]))+"";
+		}
+		else if(calculate[1].equals("/")) {
+			return (Double.parseDouble(calculate[0]) / Double.parseDouble(calculate[2]))+"";
+		}
+		else {
+			return "invalid operator";
+		}
+		
+	}
+	public String abbreviatedCellText() {
+		return (getDoubleValue() +"          ").substring(0, 10);
+		// text for spreadsheet cell display, must be exactly length 10
 	}
 }
