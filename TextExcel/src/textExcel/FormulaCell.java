8d001f8ce4a431e374a3b44d3855593ae1c384a0
APCS3rd <APCS3rd@MHS-Math2-21>
2019-04-16 11:57:15 -0700

    Work on check 5

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -31,6 +31,7 @@ public class FormulaCell extends RealCell {
 		for(int j=1;j<longInput.length;j++) {
 			if(longInput[j].charAt(0) >= 65 && longInput[j].charAt(0) <= 76) {
 				SpreadsheetLocation refCell = new SpreadsheetLocation(longInput[j].toString());
+				System.out.println((accessCell.getCell(refCell)).abbreviatedCellText()+"");
 				longInput[j] = ((RealCell) (accessCell.getCell(refCell))).getDoubleValue()+"";
 				}
 			}
